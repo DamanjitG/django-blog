3b30ebb2a6f9dc56e05dbbe935fc1a5ef438c039
Damanjit Gill <1gilldam2@gmail.com>
2021-08-25 21:40:34 -0400

    barebones django app

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # django-blog
-A simple django blog application, built as a learning project.
+A simple django blog application, built as a learning project. Credit to [Corey Schafer](https://www.youtube.com/channel/UCCezIgC97PvUuR4_gbFUs5g) for his educational youtube videos & guides.
---
+++ django_blog/blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'This is some content for the first post',
+        'date_posted': 'Aug 25 2021'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'This is some content for the second post',
+        'date_posted': 'Jan 1 2009'
+    },
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
