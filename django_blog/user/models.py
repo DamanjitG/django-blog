e078c381ceb0a1a210917fb5ed571be505455480
Damanjit Gill <1gilldam2@gmail.com>
2021-08-27 15:18:43 -0400

    users can update profiles

--- django_blog/user/models.py
+++ django_blog/user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}\'s Profile'
+    
+    def save(self):
+        super().save()
+        img = Image.open(self.pfp.path)
+
+        if img.height > 400 or img.width > 400:
+            img.thumbnail((400, 400))
+            img.save(self.pfp.path)
+
+
--- django_blog/user/views.py
+++ django_blog/user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from user.forms import UserRegistrationForm
+from user.forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 def registration(request):
     if request.method == 'POST':
@@ -17,4 +17,21 @@ def registration(request):
 
 @login_required
 def profile(request):
-    return render(request, 'user/profile.html')
+    if request.method == 'POST':
+        u_update_form = UserUpdateForm(request.POST, instance=request.user)
+        p_update_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_update_form.is_valid() and p_update_form.is_valid():
+            u_update_form.save()
+            p_update_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_update_form = UserUpdateForm(instance=request.user)
+        p_update_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_update_form': u_update_form,
+        'p_update_form': p_update_form
+    }
+
+    return render(request, 'user/profile.html', context)
