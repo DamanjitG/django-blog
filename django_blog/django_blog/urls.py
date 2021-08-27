dd4fe462ccdca5f6b923a2c34213066ba27ea79d
Damanjit Gill <1gilldam2@gmail.com>
2021-08-27 00:48:28 -0400

    automatic profile creation + basic profile page

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import  views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from user import views as user_views
 
 urlpatterns = [
@@ -26,3 +28,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
